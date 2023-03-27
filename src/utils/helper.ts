8e28fbaac9efd0798a3f3b776e795c46c5d21ab1
Umesh <umesh@codewalnut.com>
2023-03-27 18:48:35 +0530

    test git push issue

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -6,7 +6,7 @@ const validateEmail = (email: string) => {
 const getIdArrays = (data: any) => data.map((x: any) => x.id);
 
 const dateFormat = (_date: string) => {
-  const date = new Date(_date); // Create a new Date object with the current date and time
+  const date = new Date(_date); // Create a new Date object with the current date and time.
   const day = date.getDate().toString().padStart(2, '0'); // Get the day of the month as a two-digit string (e.g. "03")
   const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Get the month as a two-digit string (e.g. "07"), adding 1 to account for 0-based indexing
   const year = date.getFullYear().toString(); // Get the year as a four-digit string (e.g. "2023")
