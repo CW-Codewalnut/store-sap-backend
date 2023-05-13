5b5fcb993f0217fc7eb214e4931929777f54ce4e
Umesh <umesh@codewalnut.com>
2023-05-13 17:12:02 +0530

    fix: removed test report

--- sonar-project.js
+++ sonar-project.js
@@ -15,7 +15,6 @@ sonarqubeScanner(
       'sonar.inclusions': '**', // Entry point of your code
       'sonar.test.inclusions': 'test/**/*.test.js', // Test files
       'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info', // LCOV coverage file (you should generate this file with a test runner like Jest)
-      'sonar.testExecutionReportPaths': 'reports/test-report.xml', // Test report file (JUnit formatted)
       'sonar.projectKey': 'store-sap-backend',
       'sonar.projectName': 'store-sap-backend',
       'sonar.organization': 'codewalnut',
